b5bfa4203c603ca7cdb0394e6704d569c9993608
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2023-03-11 13:43:20 -0500

    Mejorado

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,8 +3,8 @@ import '../Style-sheets/card.css'
 function Card(props) {
     return(
         <div className='card' style={{backgroundColor: props.fondo}}>
-            <img src={props.img} className='card__img'/>
-            <h2 className='card__title'>{props.title}</h2>
+            <img src={props.img} className='card__img' alt={`Imagen ${props.title}`}/>
+            <h1 className='card__title' sr-only>{props.title}</h1>
             <p className='card__paragraph'>{props.paragraph}</p>
             <button className='card__button' style={{color: props.fondo}}>Learn More</button>
         </div>
